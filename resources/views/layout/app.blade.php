8f2c17bd79f89f7e267e054c47221c47cbd398c8
kshitiz-shresth <er.kshitizshrestha@gmail.com>
2020-09-21 00:02:41 +0545

    facebook added

--- resources/views/layout/app.blade.php
+++ resources/views/layout/app.blade.php
@@ -5,7 +5,8 @@
 		<meta charset="utf-8" />
 		<meta name="author" content="www.frebsite.nl" />
 		<meta name="viewport" content="width=device-width, initial-scale=1.0, minimum-scale=1.0, maximum-scale=1.0, user-scalable=no" />
-		
+		<meta property="fb:pages" content="321327421885896" />
+
         <title>@yield('title')</title>
         
         @include('includes.css')
