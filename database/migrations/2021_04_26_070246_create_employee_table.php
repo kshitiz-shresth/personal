28f5c1cb58f8c97d41b22f3546d7a0247133ff1f
kshitiz-shresth <er.kshitizshrestha@gmail.com>
2021-04-26 12:58:48 +0545

    new push

---
+++ database/migrations/2021_04_26_070246_create_employee_table.php
@@ -0,0 +1,37 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateEmployeeTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('employee', function (Blueprint $table) {
+            $table->id();
+            $table->timestamps();
+            $table->string('name')->nullable();
+            $table->string('passport_number')->nullable();
+            $table->string('father_name')->nullable();
+            $table->string('address')->nullable();
+            $table->string('finger')->nullable();
+            $table->string('finger_print')->nullable();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('employee');
+    }
+}
--- routes/api.php
+++ routes/api.php
@@ -14,6 +14,14 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
+// validating users
+Route::get('/users',function(){
+    return 'done';
+});
+
+
+// getting employee
+
 Route::middleware('auth:api')->get('/user', function (Request $request) {
     return $request->user();
 });
